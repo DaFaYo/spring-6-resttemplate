a6316b757a81438ba980d3d2676eda9d4f9f194d
dafayo <danielfarukyounis@gmail.com>
2025-03-16 00:07:21 +0100

    test list beer with query parameters

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -81,6 +81,25 @@ public class BeerClientMockTest {
     }
 
     @Test
+    void testListBeersWithQueryParam() throws JsonProcessingException {
+        String response = objectMapper.writeValueAsString(getPage());
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(URL + BeerClientImpl.GET_BEER_PATH)
+                .queryParam("beerName", "ALE")
+                .build().toUri();
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestTo(uri))
+                .andExpect(queryParam("beerName", "ALE"))
+                .andRespond(withSuccess(response, MediaType.APPLICATION_JSON));
+
+        Page<BeerDTO> responsePage = beerClient
+                .listBeers("ALE", null, null, null, null);
+
+        assertThat(responsePage.getContent().size()).isEqualTo(1);
+    }
+
+    @Test
     void testGetById() throws JsonProcessingException {
         mockGetOperation();
 
@@ -143,6 +162,7 @@ public class BeerClientMockTest {
 
         server.verify();
     }
+
 
     private void mockGetOperation() {
         server.expect(method(HttpMethod.GET))
